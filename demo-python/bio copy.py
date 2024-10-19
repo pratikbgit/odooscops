561a1f3884ab4a4a1d2b41681afbfef35070092f
Pratik Bachchhe <pratik6643@gmail.com>
2024-10-19 21:03:05 +0530

    Thee file copied

---
+++ demo-python/bio copy.py
@@ -0,0 +1,29 @@
+
+# def greet(name):
+#     return f"Hello, {name}!"
+
+# name = "Alice"
+# print(greet(name))
+
+
+def bio(name,age,height,is_student):
+    return f"Hello, {name}, Age is {age}, Height is + {height}, Student {is_student}"
+
+# Example:
+name = "Sandeep Bachchhe"     # String
+age = 35          # Integer
+height = 5.9      # Float
+is_student = True # Boolean
+
+print(bio(name,age,height,is_student))
+
+# def bio(name, age, height, is_student):
+#     return f"Hello, {name}. Age is {age}. Height is {height}. Student: {is_student}"
+
+# # Example:
+# name = "Pratik Bachchhe"     # String
+# age = 35          # Integer
+# height = 5.9      # Float
+# is_student = True # Boolean
+
+# print(bio(name, age, height, is_student))
---
+++ demo-python/myscript copy.py
@@ -0,0 +1,5 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+name = "Alice"
+print(greet(name))
