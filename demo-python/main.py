51dc3ce3a9b729327789cacfbd443fa45ef16f8e
Pratik Bachchhe <pratik6643@gmail.com>
2024-10-19 20:56:03 +0530

    3 files added

---
+++ demo-python/main.py
@@ -0,0 +1,53 @@
+# x = 5   # integer
+# y = "Jay Bhavani"  # string
+# z = 3.14   # float
+
+
+# print(x)
+# print(y)
+# print(z)
+
+# print (x+z)
+
+
+# name = "Python"
+
+# my_list = [1, 2, 3, "apple"]
+# my_tuple = (1, 2, 3)
+
+# my_dict = {"name": "Alice", "age": 25}
+
+# ## Conditional
+# age = 18
+# if age >= 18:
+#     print("You are an adult.")
+# else:
+#     print("You are a minor.")
+
+
+# ## for looop
+# for i in range(5):
+#     print(i)
+
+
+# #  while loop
+# i = 10
+# while i > 5:
+#     print(i)
+#     i -= 1
+
+# Creating the function
+# def greet(name):
+#     print(f"Hello, {name}")
+# greet("Alice")
+
+# def getName(mname,gname):
+#     print(f,s, "Pratik,{mname} {gname}")
+# getName("Raghurao", "Bachchhe")
+
+# def getName(mname, gname):
+#     print(f"Pratik, {mname} {gname}")
+# getName("Raghurao", "Bachchhe")
+
+name = input("Enter your name: ")
+print(f"Hello Sir , {name} how may i help you ?")
